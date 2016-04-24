adce57b34306f0af1a467e68b5920c7683bd3253
Justin Mullis <justin.mullis@gmail.com>
2016-04-24 14:52:04 -0400

    account editting and cancelation

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,9 +1,9 @@
 module LoginMacros
   def sign_in(user)
     visit new_user_session_path
-    fill_in "email", with: user.email
-    fill_in "password", with: user.password
-    click_button 'Logon'
+    fill_in "user[email]", with: user.email
+    fill_in "user[password]", with: user.password
+    click_button 'Log in'
   end
 
   # def sign_out
