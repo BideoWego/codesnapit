840fe6228aa6aaff4438ed65ca2f5c5ad7ada2df
Justin Mullis <justin.mullis@gmail.com>
2016-05-02 20:44:25 -0400

    remove test upload files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,4 +93,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.after(:suite) do
+    FileUtils.rm_rf(Dir["#{Rails.root}/public/system/profiles"])
+  end
+
 end
