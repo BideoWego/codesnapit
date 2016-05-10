165ac797873c9d3b9efc3e27a4588dfc4fce38a6
Sia <siakaramalegos@gmail.com>
2016-05-10 12:00:55 -0500

    Adds follow model, relations, and validations

---
+++ app/models/follow.rb
@@ -0,0 +1,8 @@
+class Follow < ActiveRecord::Base
+  belongs_to :initiator, foreign_key: :initiator_id, class_name: 'User'
+  belongs_to :following, foreign_key: :following_id, class_name: 'User'
+
+  validates_presence_of :initiator, :following
+  validates_uniqueness_of :following_id, scope: :initiator_id
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ActiveRecord::Base
   has_many :snap_it_proxies, :dependent => :destroy
   has_many :snap_its, :dependent => :destroy
 
+  has_many :following_relations, foreign_key: :initiator_id, class_name: 'Follow', dependent: :destroy
+  has_many :following, through: :following_relations, source: :following
+
+  has_many :follower_relations, foreign_key: :following_id, class_name: 'Follow', dependent: :destroy
+  has_many :followers, through: :follower_relations, source: :initiator
+
   before_create :build_profile
 
   validates :username, length: { in: 3..12 }
