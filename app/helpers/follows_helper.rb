d59a3253ebff6dd8f626ba888eca48a24cd265cf
Sia <siakaramalegos@gmail.com>
2016-05-10 17:37:09 -0500

    On hover, following button changes to unfollow

--- app/helpers/follows_helper.rb
+++ app/helpers/follows_helper.rb
@@ -4,9 +4,9 @@ module FollowsHelper
     if current_user == user
       button_string = ''
     elsif current_user.following.include?(user)
-      button_string = "#{link_to fa_icon('minus', text: 'Unfollow'), follow_path(following: @profile.user.id), method: :delete, class: 'btn btn-default'}"
+      button_string = "#{link_to 'Following', follow_path(following: @profile.user.id), method: :delete, class: 'btn btn-primary following-button'}"
     else
-      button_string = "#{link_to fa_icon('plus', text: 'Follow'), follow_path(following: @profile.user.id), method: :post, class: 'btn btn-primary'}"
+      button_string = "#{link_to fa_icon('plus', text: 'Follow'), follow_path(following: @profile.user.id), method: :post, class: 'btn btn-default'}"
     end
 
     button_string.html_safe
