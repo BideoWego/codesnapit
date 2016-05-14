6699426e9ff9eb117d54614b81e1f24c21fc75ce
Bideo Wego <bideowego@gmail.com>
2016-05-14 19:46:03 -0400

    Move link to create snapit form to navbar center

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,15 @@ module ApplicationHelper
   end
 
 
+  def logo_url(style=:normal)
+    style = {
+      :normal => '',
+      :white => '-white'
+    }[style]
+    "#{root_url}images/logo#{style}.svg"
+  end
+
+
   def flash_css_class(key)
     {
       'notice' => 'info',
